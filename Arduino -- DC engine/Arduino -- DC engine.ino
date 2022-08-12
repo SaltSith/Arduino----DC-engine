51becb5153d5af4d73e6efa9a4819b9ebeb2f6fc
SaltSith <SaltSith66@gmail.com>
2022-08-12 22:50:46 +0200

    create two dc_engine objects

--- Arduino -- DC engine/Arduino -- DC engine.ino
+++ Arduino -- DC engine/Arduino -- DC engine.ino
@@ -4,12 +4,33 @@
  Author:	marcin
 */
 
+#include "dc_engine.h"
+
+dc_engine engine_single_direction(1);
+dc_engine engine_bi_direction(2, 3);
+
+
 // the setup function runs once when you press reset or power the board
 void setup() {
-
+	engine_single_direction.init();
+	engine_bi_direction.init();
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+	engine_single_direction.start();
+	engine_bi_direction.start(DC_ENGINE_FORWARD);
+	delay(1000);
+
+	engine_single_direction.stop();
+	engine_bi_direction.stop();
+	delay(1000);
+
+	engine_single_direction.start();
+	engine_bi_direction.start(DC_ENGINE_BACKWARD);
+	delay(1000);
+
+	engine_single_direction.stop();
+	engine_bi_direction.stop();
+	delay(1000);
 }
