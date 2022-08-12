6e45768124c1331f7a18c5af36e73ca04fac25d2
SaltSith <SaltSith66@gmail.com>
2022-08-12 22:24:14 +0200

    Add project files.

---
+++ Arduino -- DC engine/Arduino -- DC engine.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Arduino____DC_engine.ino
+ Created:	8/12/2022 10:24:04 PM
+ Author:	marcin
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
